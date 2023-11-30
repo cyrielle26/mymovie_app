367761311c4359b843dc84bf27473e57c8c3df9b
cyrielle26 <cyrielleperminjat@gmail.com>
2023-11-30 15:51:33 +0900

    footer end

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,3 +1,18 @@
+import styled from "styled-components";
+
+const Container = styled.footer`
+padding: 100px;
+margin-top: 80px;
+display: flex;
+justify-content: center;
+align-items: center;
+border-top: 1px solid rgba(255,255,255, 0.6);`
+
+
 export const Footer = () => {
-    return <div></div>
-}
+    return (
+      <Container>
+        &copy; 2023 Cpcoding
+      </Container>
+    );
+  };
