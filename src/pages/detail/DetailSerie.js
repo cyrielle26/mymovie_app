d31432692d62df68d4d10468e6267b1d1eabe71e
cyrielle26 <cyrielleperminjat@gmail.com>
2023-12-04 02:10:21 +0900

    media query in progress

--- src/pages/detail/DetailSerie.js
+++ src/pages/detail/DetailSerie.js
@@ -9,16 +9,43 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faStar } from "@fortawesome/free-regular-svg-icons";
 
 const Container = styled.div`
+  position: relative;
   width: 60%;
   margin: 100px 20% 0px 20%;
   padding: 0 55px;
   display: flex;
   justify-content: space-between;
   flex-direction: column;
+  @media screen and (max-width: 1200px) {
+    width: 70%;
+    margin: 85px 16% 0px 16%;
+    font-size: 16px;
+  }
+
+  @media screen and (max-width: 600px) {
+    width: 90%;
+    margin: 70px 5% 0 5%;
+    padding: 0 45px;
+  }
+
+  @media screen and (max-width: 480px) {
+    width: 100%;
+  }
 `;
 const BgWrap = styled.div`
   width: 100%;
   height: 600px;
+  @media screen and (max-width: 1200px) {
+    height: 500px;
+  }
+
+  @media screen and (max-width: 1024px) {
+    height: 300px;
+  }
+
+  @media screen and (max-width: 480px) {
+    height: 350px;
+  }
 `;
 
 const Bg = styled.div`
@@ -34,20 +61,63 @@ const InfoWrap = styled.div`
   height: 100%;
   font-size: 16px;
   border-bottom: 1px solid white;
+  @media screen and (max-width: 1200px) {
+    max-height: 70%;
+  }
+
+  @media screen and (max-width: 900px) {
+    max-height: 60%;
+    font-size: 14px;
+  }
+
+  @media screen and (max-width: 480px) {
+    max-height: 50%;
+  }
 `;
 
 const TitleWrap = styled.div`
+  margin-top: 40px;
   width: 100%;
   line-height: 30px;
   height: 25%;
   display: flex;
   justify-content: space-between;
+  @media screen and (max-width: 1200px) {
+    margin-top: 10px;
+    line-height: 24px;
+    height: 10%;
+  }
+
+  @media screen and (max-width: 1024px) {
+    font-size: 16px;
+  }
+
+  @media screen and (max-width: 900px) {
+    margin-top: 50px;
+  }
+
+  @media screen and (max-width: 480px) {
+    font-size: 14px;
+    margin-top: 10px;
+    line-height: 15px;
+  }
 `;
 
 const SerieTitle = styled.h3`
   font-size: 30px;
   font-weight: 900;
   margin-bottom: 30px;
+  @media screen and (max-width: 1200px) {
+    font-size: 24px;
+  }
+
+  @media screen and (max-width: 1024px) {
+    font-size: 20px;
+  }
+
+  @media screen and (max-width: 480px) {
+    font-size: 14px;
+  }
 `;
 
 const Rating = styled.h3`
@@ -55,10 +125,25 @@ const Rating = styled.h3`
   font-size: 20px;
   font-weight: 400;
   float: right;
+  @media screen and (max-width: 1024px) {
+    font-size: 18px;
+  }
+
+  @media screen and (max-width: 900px) {
+  }
+  @media screen and (max-width: 768px) {
+  }
+  @media screen and (max-width: 600px) {
+  }
+
+  @media screen and (max-width: 480px) {
+  }
+
+  @media screen and (max-width: 320px) and (min-width: 300px) {
+  }
 `;
 
 const Genre = styled.ul`
-  float: right;
   padding: 70px;
   font-size: 20px;
   li {
@@ -66,10 +151,52 @@ const Genre = styled.ul`
     margin-left: 20px;
     margin-bottom: 10px;
   }
+  @media screen and (max-width: 1200px) {
+    padding: 50px;
+  }
+
+  @media screen and (max-width: 1024px) {
+    font-size: 16px;
+    padding: 40px;
+  }
+
+  @media screen and (max-width: 900px) {
+    font-size: 14px;
+    position: absolute;
+    bottom: 0;
+    right: 0;
+    padding: 50px;
+    margin: 50px;
+  }
+  @media screen and (max-width: 768px) {
+  }
+  @media screen and (max-width: 600px) {
+  }
+
+  @media screen and (max-width: 480px) {
+  }
+
+  @media screen and (max-width: 320px) (min-width: 300px) {
+  }
 `;
 
 const Release = styled.div`
   margin-top: 50px;
+
+  @media screen and (max-width: 900px) {
+    margin-top: 20px;
+  }
+
+  @media screen and (max-width: 768px) {
+  }
+  @media screen and (max-width: 600px) {
+  }
+
+  @media screen and (max-width: 480px) {
+  }
+
+  @media screen and (max-width: 320px) and (min-width: 300px) {
+  }
 `;
 
 const Runtime = styled.div``;
@@ -84,6 +211,19 @@ const Description = styled.p`
   font-weight: 400;
   line-height: 2em;
   margin-bottom: 50px;
+  @media screen and (max-width: 900px) {
+    margin-top: 20px;
+  }
+  @media screen and (max-width: 768px) {
+  }
+  @media screen and (max-width: 600px) {
+  }
+
+  @media screen and (max-width: 480px) {
+  }
+
+  @media screen and (max-width: 320px) (min-width: 300px) {
+  }
 `;
 
 const Season = styled.div`
