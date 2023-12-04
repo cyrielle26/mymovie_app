6f5d29871263ae0b24404aab738efcecc0c2645b
cyrielle26 <cyrielleperminjat@gmail.com>
2023-12-04 22:10:54 +0900

    genre list style @mesiqauery done

--- src/components/GenreList.js
+++ src/components/GenreList.js
@@ -12,12 +12,25 @@ const Container = styled.div`
   padding: 135px 55px;
   width: 100%;
   height: 80vh;
+  @media screen and (max-width: 320px) and (min-width: 300px) {
+    padding: 130px 25px;
+  }
 `;
 
 const Title = styled.h3`
   font-size: 22px;
   font-weight: 800;
   margin-bottom: 55px;
+  @media screen and (max-width: 480px) {
+    font-size: 20px;
+  }
+  @media screen and (max-width: 320px) and (min-width: 300px) {
+    font-weight: 900;
+    padding-right: 25px;
+    letter-spacing: 1px;
+    line-height: 20px;
+    margin-bottom: 30px;
+  }
 `;
 
 const Button = styled.button`
@@ -32,22 +45,62 @@ const Button = styled.button`
   opacity: 0.8;
   border: 1px solid #f9f9f9;
   padding: 5px;
+
+  @media screen and (max-width: 480px) {
+    border-radius: 18px;
+    padding: 10px;
+  }
+
+  @media screen and (max-width: 320px) and (min-width: 300px) {
+    border-radius: 25px;
+  }
 `;
 
 const SubTitle = styled.h3`
   margin-top: 30px;
   font-size: 18px;
   font-weight: 500;
+  @media screen and (max-width: 480px) {
+    font-size: 18px;
+  }
+  @media screen and (max-width: 320px) and (min-width: 300px) {
+    font-size: 18px;
+    font-weight: 800;
+    margin-right: 25px;
+    margin-top: 15px;
+  }
 `;
 const GenreWrap = styled.ul`
   margin-top: 45px;
   margin-bottom: 40px;
-  width: 100%;
+  width: 85%;
   display: flex;
   flex-wrap: wrap;
   justify-content: space-between;
   align-items: flex-start;
-  gap: 18px;
+  gap: 8px;
+  @media screen and (max-width: 1200px) {
+    width: 75%;
+    justify-content: flex-start;
+  }
+
+  @media screen and (max-width: 900px) {
+    width: 90%;
+    margin-top: 30px;
+  }
+  @media screen and (max-width: 600px) {
+    width: 100%;
+  }
+
+  @media screen and (max-width: 480px) {
+    gap: 10px;
+    margin-top: 38px;
+  }
+
+  @media screen and (max-width: 320px) and (min-width: 300px) {
+    gap: 15px;
+    margin-top: 20px;
+  }
 `;
 
 const PosterWrap = styled.div`
